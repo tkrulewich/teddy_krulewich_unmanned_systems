70a5e9f061ceb2ba57e5ccd426d2472f1e45e019
Edward Krulewich <ekkyv6@umsystem.edu>
2022-08-30 11:48:36 -0500

    Implemented djikstras

--- teddy_krulewich_unmanned_systems/week_2/week2.py
+++ teddy_krulewich_unmanned_systems/week_2/week2.py
@@ -1,5 +1,3 @@
-from cmath import sqrt
-from tokenize import Double
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -13,10 +11,13 @@ class Node:
         self.parent_node = parent_node
         self.index = index
 
-        self.cost = -1
+        self.cost = math.inf
     
     def distance(self, other):
         return math.sqrt( (self.x - other.x)**2 + (self.y - other.y) ** 2 )
+    
+    def __lt__(self, other):
+        return self.cost < other.cost
 
 
 class Obstacle:
@@ -98,6 +99,10 @@ class Grid:
     
     def get_node(self, x, y):
         """Gets the node instance closest to a given x,y coodinate"""
+
+        if not (self.min_x <= x <= self.max_x and self.min_y <= y <= self.max_y):
+            return None
+        
         return self.Nodes[ int(self.get_node_index(x, y) )]
 
     def draw(self):
@@ -107,24 +112,92 @@ class Grid:
             obstacle.draw()
         
         for node in self.Nodes:
-            plt.text(node.x, node.y, str(node.index), color="red", fontsize=8, horizontalalignment="center", verticalalignment = "center")
+            plt.text(node.x, node.y, str(round(node.cost, 2)), color="red", fontsize=8, horizontalalignment="center", verticalalignment = "center")
         
         plt.xlim([self.min_x - self.spacing, self.max_x + self.spacing])
         plt.ylim([self.min_y - self.spacing, self.max_y + self.spacing])
         plt.xticks(ticks = [ x for x in np.arange(self.min_x, self.max_x + self.spacing, self.width / 5.0)])
         plt.yticks(ticks = [ y for y in np.arange(self.min_y, self.max_y + self.spacing, self.height / 5.0)])
-        plt.show()
     
     def add_obstacle(self, obstacle: Obstacle):
         self.Obstacles.append(obstacle)
     
+    def add_obstacles(self, obstacles: list):
+        self.Obstacles.extend(obstacles)
+
+    def djikstras(self, start, end):
+
+        start.cost = 0
+
+        current_node : Node = start
+
+        unvisited = set(self.Nodes)
+        visited = set()
+
+        while len(unvisited) > 0:
+            unvisited.remove(current_node)
+            visited.add(current_node)
+            
+            for y in np.arange(current_node.y - self.spacing, current_node.y + 2 * self.spacing, self.spacing):
+                for x in np.arange(current_node.x - self.spacing, current_node.x + 2 * self.spacing, self.spacing):
+                    
+                    neighbor = self.get_node(x, y)
+
+                    if (self.node_valid(neighbor) and neighbor is not current_node):
+                        cost = current_node.cost + current_node.distance(neighbor)
+                        if (cost < neighbor.cost):
+                            neighbor.cost = cost
+                            neighbor.parent_node = current_node
+            
+            if len(unvisited) > 0:
+                current_node = min(unvisited, key=lambda x: x.cost)
+            
+        x_list = []
+        y_list = []
+
+        current_node = end
+        while current_node != None:
+            x_list.append(current_node.x)
+            y_list.append(current_node.y)
+
+            current_node = current_node.parent_node
+        
+        return x_list, y_list
+    
+    def node_valid(self, node: Node):
+        if node is None:
+            return False
+
+        for obstacle in self.Obstacles:
+            if obstacle.CollidesWith(node):
+                return False
+        
+        return True
+        
+
 
 grid = Grid(0, 10, 0, 10, 0.5)
 
-grid.add_obstacle( Obstacle(2, 2, 0.5))
+grid.add_obstacles([
+    Obstacle(5, 5, 5), 
+    Obstacle(3, 4, 1.5),
+    Obstacle(5, 0, 1.5),
+    Obstacle(5, 1, 2.5),
+    Obstacle(0, 7, 0.5),
+    Obstacle(1, 7, 0.5),
+    Obstacle(2, 7, 0.5),
+    Obstacle(3, 7, 1.5)])
 
+
+path = grid.djikstras( grid.get_node(0, 0), grid.get_node(8,9))
 grid.draw()
-#grid.draw_grid_with_indicies()
+
+for node in path:
+    x, y = path
+
+    plt.plot(x, y)
+
+plt.show()
 
 node = grid.get_node_index(8, 4.5)
 
