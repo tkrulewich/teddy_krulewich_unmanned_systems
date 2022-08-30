587898ccc18b8cde07af614505cce8ee59f07aed
Edward Krulewich <ekkyv6@umsystem.edu>
2022-08-29 19:30:36 -0500

    add comments and docstrings

--- teddy_krulewich_unmanned_systems/week_1/week1.py
+++ teddy_krulewich_unmanned_systems/week_1/week1.py
@@ -9,8 +9,10 @@ class Node:
     def __init__(self, x, y, parent_node, index):
         self.x = x
         self.y = y
+
         self.parent_node = parent_node
         self.index = index
+        
         self.cost = -1
     
     def distance(self, other):
@@ -19,7 +21,6 @@ class Node:
 
 class Grid:
     def __init__(self, min_x : float, max_x : float, min_y : float, max_y : float, spacing : float):
-        
         self.min_x = min_x
         self.max_x = max_x
 
@@ -41,9 +42,13 @@ class Grid:
                 index += 1
     
     def get_node_index(self, x: float, y : float):
-
+        """Gets index of node closest to an x,y coordinate"""
+        
+        # if the node isn't on a grid point, use the closest x,y coordinate that is on the grid
         if (x % self.spacing != 0 or y % self.spacing != 0):
-            neighbors = []
+
+            # there are four possible closest points found by all combinations of
+            # the x and y coordinates rounded up and down
 
             floor_x = x - x % self.spacing
             floor_y = y - y % self.spacing
@@ -51,8 +56,10 @@ class Grid:
             ceil_x = floor_x + self.spacing
             ceil_y = floor_y + self.spacing
 
+            # the list of possible closest points
             neighbors = [ (floor_x, floor_y), (ceil_x, floor_y), (floor_x, ceil_y), (ceil_x, ceil_y) ]
 
+            # find the closest
             min_dist = math.inf
             min_x, min_y = math.inf, math.inf
             for x2,y2 in neighbors:
@@ -62,16 +69,22 @@ class Grid:
                     min_x = x2
                     min_y = y2
             
+            # set x and y to the coordidnates of the closest point found
             x = min_x
             y = min_y
-
+        
+        # find the index of the point and return it
         index = (y - self.min_y) / self.spacing * (self.max_x / self.spacing + 1) + x / self.spacing
         return int(index)
     
+    
     def get_node(self, x, y):
+        """Gets the node instance closest to a given x,y coodinate"""
         return self.Nodes[ int(self.get_node_index(x, y) )]
 
     def draw_grid_with_indicies(self):
+        """draws the grid with node indicies displayed in their corresponding (x, y) coordinates"""
+
         for node in self.Nodes:
             plt.text(node.x, node.y, str(node.index), color="red", fontsize=8)
         
@@ -85,4 +98,5 @@ grid = Grid(0, 15, 0, 10, 0.5)
 #grid.draw_grid_with_indicies()
 
 node = grid.get_node_index(8, 4.5)
+
 print(node)
