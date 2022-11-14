c8bfd4c66f9ff40745d9a2af180b5c050ee0646c
Edward Krulewich <ekkyv6@mail.umkc.edu>
2022-11-13 19:00:17 -0600

    created a launcher for exam

--- teddy_krulewich_unmanned_systems/exam/src/exam/setup.py
+++ teddy_krulewich_unmanned_systems/exam/src/exam/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'exam'
 submodules = 'exam.submodules'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'question2 = exam.question2:main'
+            'question2 = exam.question2:main',
+            'turtlebot_spawn = exam.turtlebot_spawn:main',
         ],
     },
 )
